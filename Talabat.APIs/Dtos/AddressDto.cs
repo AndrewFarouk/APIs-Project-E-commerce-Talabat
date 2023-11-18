cb69239f82a8bdf40740c3e09eee99c18e3b1fd8
AndrewFarouk <104110420+AndrewFarouk@users.noreply.github.com>
2023-11-18 17:35:03 +0200

    Update AddressDto.cs

--- Talabat.APIs/Dtos/AddressDto.cs
+++ Talabat.APIs/Dtos/AddressDto.cs
@@ -6,17 +6,17 @@ namespace Talabat.APIs.Dtos
     {
         [Required]
         public string FirstName { get; set; }
-
+        
         [Required]
         public string LastName { get; set; }
+        
         [Required]
         public string Street { get; set; }
-
+        
         [Required]
         public string City { get; set; }
-
+        
         [Required]
         public string Country { get; set; }
-
     }
 }
