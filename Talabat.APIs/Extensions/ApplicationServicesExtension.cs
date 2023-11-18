663642cd936160a3ef98664b8b55914409ca252b
AndrewFarouk <104110420+AndrewFarouk@users.noreply.github.com>
2023-11-18 17:36:50 +0200

    Update ApplicationServicesExtension.cs

--- Talabat.APIs/Extensions/ApplicationServicesExtension.cs
+++ Talabat.APIs/Extensions/ApplicationServicesExtension.cs
@@ -14,13 +14,10 @@ namespace Talabat.APIs.Extensions
         public static IServiceCollection AddApplicationSerives(this IServiceCollection services)
         {
             services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
-            //services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-            //services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped(typeof(IBasketRepository), typeof(BasketRepository));
             services.AddScoped(typeof(IOrderService), typeof(OrderService));
             services.AddScoped(typeof(IPaymentService), typeof(PaymentService));
             services.AddSingleton<IResponseCacheService, ResponseCacheService>();
-            //builder.Services.AddAutoMapper(M => M.AddProfile(new MappingProfiles()));
             services.AddAutoMapper(typeof(MappingProfiles));
 
             #region Error Handling
@@ -28,10 +25,6 @@ namespace Talabat.APIs.Extensions
                 {
                     Options.InvalidModelStateResponseFactory = (actionContext) =>
                     {
-                        // ModelState => Dic [KeyValuePair]
-                        // Key => Name of Param
-                        // Value => Errors
-
                         var errors = actionContext.ModelState.Where(P => P.Value.Errors.Count() > 0)
                                                   .SelectMany(P => P.Value.Errors)
                                                   .Select(E => E.ErrorMessage)
