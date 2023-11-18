972bf8e6411f057ee29d70916f5c8a9141ceecb8
AndrewFarouk <104110420+AndrewFarouk@users.noreply.github.com>
2023-11-18 17:35:23 +0200

    Update BasketItemDto.cs

--- Talabat.APIs/Dtos/BasketItemDto.cs
+++ Talabat.APIs/Dtos/BasketItemDto.cs
@@ -6,17 +6,23 @@ namespace Talabat.APIs.Dtos
     {
         [Required]
         public int Id { get; set; }
+        
         [Required]
         public string ProductName { get; set; }
+        
         [Required]
         public string PictureUrl { get; set; }
+        
         [Required]
         [Range(0.1, double.MaxValue, ErrorMessage = "Price can not be Zero")]
         public decimal Price { get; set; }
+        
         [Required]
         public string Brand { get; set; }
+        
         [Required]
         public string Type { get; set; }
+        
         [Required]
         [Range(1, int.MaxValue, ErrorMessage ="Quantity Must Be One Item at Least")]
         public int Quantity { get; set; }
