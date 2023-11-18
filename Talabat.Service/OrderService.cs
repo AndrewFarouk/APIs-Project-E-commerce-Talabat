df0b335ed5ee5f9a183ccfd020fdce20da488bd6
AndrewFarouk <andrewmorks@gmail.com>
2023-11-18 17:27:58 +0200

    Update Talabat.Service

---
+++ Talabat.Service/OrderService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core;
+using Talabat.Core.Entities;
+using Talabat.Core.Entities.Order_Aggregate;
+using Talabat.Core.Repositories;
+using Talabat.Core.Service;
+using Talabat.Core.Specifications.Order_Specifications;
+
+namespace Talabat.Service
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IBasketRepository _basket;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IPaymentService _paymentService;
+
+        public OrderService(IBasketRepository basket,
+                            IUnitOfWork unitOfWork,
+                            IPaymentService paymentService)
+        {
+            _basket = basket;
+            _unitOfWork = unitOfWork;
+            _paymentService = paymentService;
+        }
+        public async Task<Order?> CreateOrderAsync(string buyerEmail, string basketId, int DeliveryMethodId, Address shippingAddress)
+        {
+            // 1.Get Basket From Basket Repo
+            var Basket = await _basket.GetBasketAsync(basketId);
+
+            // 2.Get Selected Items at Basket From Product Repo
+            var OrderItems = new List<OrderItem>();
+
+            if(Basket?.Items.Count > 0)
+            {
+                foreach(var item in Basket.Items)
+                {
+                    var Product = await _unitOfWork.Repository<Product>().GetByIdAsync(item.Id);
+                    var ProductItemOrdered = new ProductItemOrder(item.Id, Product.Name, Product.PictureUrl);
+                    var OrderItem = new OrderItem(ProductItemOrdered, Product.Price, item.Quantity);
+                    OrderItems.Add(OrderItem);
+                }
+            }
+
+            // 3.Calculate SubTotal => Price * Quantity
+            var SubTotal = OrderItems.Sum(item => item.Price * item.Quantity);
+
+            // 4.Get Delivery Method From DeliveryMethod Repo
+            var DeliveryMethod = await _unitOfWork.Repository<DeliveryMethod>().GetByIdAsync(DeliveryMethodId);
+
+            // 5.Create Order
+            var Spec = new OrderWithPaymentIntentSpec(Basket.PaymentIntentId);
+            var ExOrder = await _unitOfWork.Repository<Order>().GetByEntityWithSpecAsync(Spec);
+            if(ExOrder is not null)
+            {
+                _unitOfWork.Repository<Order>().Delete(ExOrder);
+                await _paymentService.CreateOrUpdatePaymentIntent(basketId);
+            }
+
+            var Order = new Order(buyerEmail, shippingAddress, DeliveryMethod, OrderItems, SubTotal, Basket.PaymentIntentId);
+
+            // 6.Add Order Locally
+            await _unitOfWork.Repository<Order>().AddAsync(Order);
+           
+            // 7.Save Order To Database[ToDo]
+            var Result = await _unitOfWork.CompleteAsync();
+
+            if (Result < 0) return null;
+
+            return Order;
+        }
+
+        public async Task<Order> GetOrderByIdForSpecificUserAsync(string buyerEmail, int orderId)
+        {
+            var Spec = new OrderSpecifications(buyerEmail, orderId);
+            var Order = await _unitOfWork.Repository<Order>().GetByEntityWithSpecAsync(Spec);
+            return Order;
+        }
+
+        public async Task<IReadOnlyList<Order>> GetOrdersForSpecificUserAsync(string buyerEmail)
+        {
+            var Spec = new OrderSpecifications(buyerEmail);
+            var Orders = await _unitOfWork.Repository<Order>().GetAllWithSpecAsync(Spec);
+            return Orders;
+        }
+    }
+}
---
+++ Talabat.Service/PaymentService.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Extensions.Configuration;
+using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core;
+using Talabat.Core.Entities;
+using Talabat.Core.Entities.Order_Aggregate;
+using Talabat.Core.Repositories;
+using Talabat.Core.Service;
+using Talabat.Core.Specifications.Order_Specifications;
+
+namespace Talabat.Service
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IBasketRepository _basketRepository;
+
+        public PaymentService(IConfiguration configuration,
+                              IUnitOfWork unitOfWork,
+                              IBasketRepository basketRepository)
+        {
+            _configuration = configuration;
+            _unitOfWork = unitOfWork;
+            _basketRepository = basketRepository;
+        }
+        public async Task<CustomerBasket?> CreateOrUpdatePaymentIntent(string BasketId)
+        {
+            // Secret Key
+            StripeConfiguration.ApiKey = _configuration["StripeSetting:Secretkey"];
+
+            // Get basket
+            var Basket = await _basketRepository.GetBasketAsync(BasketId);
+            if (Basket is null) return null;
+
+            //Create Payment Intent [Options = Total (SubToral + DM.Cost)]
+            var ShippingPrice = 0M;
+            if(Basket.DeliveryMethodId.HasValue)
+            {
+                var DeliveryMethod = await _unitOfWork.Repository<DeliveryMethod>().GetByIdAsync(Basket.DeliveryMethodId.Value);
+                ShippingPrice = DeliveryMethod.Cost;
+            }
+
+            //Check Price Of Items
+            if(Basket.Items.Count > 0)
+            {
+                foreach(var item in Basket.Items)
+                {
+                    var Product = await _unitOfWork.Repository<Core.Entities.Product>().GetByIdAsync(item.Id);
+                    if(item.Price != Product.Price)
+                        item.Price = Product.Price;
+                }
+            }
+
+            // Create Payment Intent
+            var Service = new PaymentIntentService();
+            PaymentIntent paymentIntent;
+
+            if (string.IsNullOrEmpty(Basket.PaymentIntentId))
+            {
+                var Options = new PaymentIntentCreateOptions()
+                {
+                    Amount = (long)Basket.Items.Sum(item => item.Price * item.Quantity * 100) + (long)ShippingPrice * 100,
+                    Currency = "usd",
+                    PaymentMethodTypes = new List<string>() { "card" }
+                };
+                paymentIntent = await Service.CreateAsync(Options);
+                Basket.PaymentIntentId = paymentIntent.Id;
+                Basket.ClientSecret = paymentIntent.ClientSecret;
+            }
+
+            else // Update
+            {
+                var Options = new PaymentIntentUpdateOptions()
+                {
+                    Amount = (long)Basket.Items.Sum(item => item.Price * item.Quantity * 100) + (long)ShippingPrice * 100
+                };
+                paymentIntent = await Service.UpdateAsync(Basket.PaymentIntentId, Options);
+                Basket.PaymentIntentId = paymentIntent.Id;
+                Basket.ClientSecret = paymentIntent.ClientSecret;
+            }
+
+            await _basketRepository.UpdateBasketAsync(Basket);
+            return Basket;
+        }
+
+        public async Task<Order> UpdatePaymentIntentToSucceedOrFailed(string PaymentIntentId, bool flag)
+        {
+            var Spec = new OrderWithPaymentIntentSpec(PaymentIntentId);
+            var Order = await _unitOfWork.Repository<Order>().GetByEntityWithSpecAsync(Spec);
+            if (flag)
+                Order.Status = OrderStatus.PaymentReceived;
+            else
+                Order.Status = OrderStatus.PaymentFailed;
+
+            _unitOfWork.Repository<Order>().Update(Order);
+            await _unitOfWork.CompleteAsync();
+            return Order;
+        }
+    }
+}
